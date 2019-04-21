7b7d3b9958830e8b2c3b0aad96c50e46183ce759
jrbeverly <jrbeverly@live.com>
2019-04-20 21:35:04 -0400

    Setup a basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,1 +1,11 @@
+module "cicd_setup" {
+  source = "../../"
 
+  username  = "infraprints-iam-ci-role-basic"
+  role_name = "infraprints-iam-ci-role-basic"
+
+  environment_variable = {
+    s3_bucket   = "infraprints-bucket-example"
+    hello_world = "hello world"
+  }
+}
