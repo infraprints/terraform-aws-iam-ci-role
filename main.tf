0e818b78125427d633c92bad53e603d23b909cba
jrbeverly <jrbeverly@live.com>
2019-06-02 13:16:14 -0400

    Uprgade to terraform 0.12

--- main.tf
+++ main.tf
@@ -2,44 +2,49 @@ locals {
   global = {
     External       = true
     Credentials    = true
-    RotationPeriod = "${var.period}"
+    RotationPeriod = var.period
   }
 
   labels = {
-    AccessKeyLabel = "${lookup(var.labels, "access_key", "AWS_ACCESS_KEY_ID")}"
-    SecretKeyLabel = "${lookup(var.labels, "secret_key", "AWS_SECRET_ACCESS_KEY")}"
-    TokenLabel     = "${lookup(var.labels, "token", "AWS_SESSION_TOKEN")}"
+    AccessKeyLabel = lookup(var.labels, "access_key", "AWS_ACCESS_KEY_ID")
+    SecretKeyLabel = lookup(var.labels, "secret_key", "AWS_SECRET_ACCESS_KEY")
+    TokenLabel     = lookup(var.labels, "token", "AWS_SESSION_TOKEN")
   }
 
-  adjusted_with_prefix = "${formatlist("ENV_%s", keys(var.environment_variable))}"
-  adjusted_env_vars    = "${zipmap(local.adjusted_with_prefix, values(var.environment_variable))}"
+  adjusted_with_prefix = formatlist("ENV_%s", keys(var.environment_variable))
+  adjusted_env_vars    = zipmap(local.adjusted_with_prefix, values(var.environment_variable))
 }
 
 resource "aws_iam_role" "external" {
-  name                  = "${var.role_name}"
-  path                  = "${var.path}"
-  assume_role_policy    = "${data.aws_iam_policy_document.role.json}"
+  name                  = var.role_name
+  path                  = var.path
+  assume_role_policy    = data.aws_iam_policy_document.role.json
   description           = "User-linked role accessible by ${var.username} for deploying resources"
-  max_session_duration  = "${var.max_session_duration}"
+  max_session_duration  = var.max_session_duration
   force_detach_policies = true
 
-  tags = "${merge(local.global, var.tags, local.labels)}"
+  tags = merge(local.global, var.tags, local.labels)
 }
 
 resource "aws_iam_role_policy_attachment" "policies" {
-  count      = "${length(var.policies)}"
-  role       = "${aws_iam_role.external.id}"
-  policy_arn = "${element(var.policies, count.index)}"
+  count      = length(var.policies)
+  role       = aws_iam_role.external.id
+  policy_arn = element(var.policies, count.index)
 }
 
 resource "aws_iam_user" "external" {
-  name = "${var.username}"
-  path = "${var.path}"
-  tags = "${merge(local.global, local.adjusted_env_vars, var.tags, local.labels)}"
+  name = var.username
+  path = var.path
+  tags = merge(
+    local.global,
+    local.adjusted_env_vars,
+    var.tags,
+    local.labels,
+  )
 }
 
 resource "random_string" "external_id" {
-  length  = "${var.length}"
+  length  = var.length
   special = false
 }
 
@@ -51,21 +56,21 @@ data "aws_iam_policy_document" "role" {
 
     principals {
       type        = "AWS"
-      identifiers = ["${aws_iam_user.external.arn}"]
+      identifiers = [aws_iam_user.external.arn]
     }
 
     condition {
       test     = "StringEquals"
       variable = "sts:ExternalId"
-      values   = ["${random_string.external_id.result}"]
+      values   = [random_string.external_id.result]
     }
   }
 }
 
 resource "aws_iam_user_policy" "external" {
   name   = "AssumeRole"
-  user   = "${aws_iam_user.external.name}"
-  policy = "${data.aws_iam_policy_document.user.json}"
+  user   = aws_iam_user.external.name
+  policy = data.aws_iam_policy_document.user.json
 }
 
 data "aws_iam_policy_document" "user" {
@@ -73,6 +78,7 @@ data "aws_iam_policy_document" "user" {
     sid       = "AllowAssumeOfRole"
     effect    = "Allow"
     actions   = ["sts:AssumeRole"]
-    resources = ["${aws_iam_role.external.arn}"]
+    resources = [aws_iam_role.external.arn]
   }
 }
+
